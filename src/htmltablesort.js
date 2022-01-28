4d61a214e029a0cd2a64a1ee88fdf3a0c30c0204
Eric Degoul <eric.degoul@gmail.com>
2022-01-28 10:43:49 -0400

    add data-sort attribute on header for def sort

--- src/htmltablesort.js
+++ src/htmltablesort.js
@@ -6,88 +6,95 @@
     Usage            : 
     
     window.onload = function(){
-        HtmlTableSort.init('id-table',0,'asc','&nbsp;&middot','&nbsp;&uarr;','&nbsp;&darr;');
+        HtmlTableSort.init('id-table');
         };
  */
 
-var HtmlTableSort = {
-    init: function( id,              // id of the table to make sortable
-                    colSorted,       // default column sorted
-                    colOrder,        // default column order 'asc' or 'desc'
-                    NotSorted,       // text to display when not sorted
-                    AscSortedArrow,  // text to display when sorted asc
-                    DescSortedArrow  // text to display when sorted desc
-                    )
-        {
-        
-        var _this  = this; // store context of this in the object
-        var table  = document.getElementById(id);
-        var th     = table.tHead, i;
-        var lastTh =false;
-
-        th && (th = th.rows[0]) && (th = th.cells);
-
-        if(th)  i = th.length;
-        else    return; // no <thead then do nothing        
-
-        // loop on <th>
-        while (--i >= 0) (function (i) {
-            var dir = 1;
-
-            // set cursor style
-            th[i].style.cursor = 'pointer';
-
-            // set & save column titles
-            th[i].setAttribute('data-caption',th[i].innerHTML);
-            if(i==colSorted) // is it the default sorted column
-                {                
-                if(colOrder=='asc')
-                    {
-                    dir = 1 - dir;
-                    th[i].innerHTML += AscSortedArrow;
-                    }
-                else th[i].innerHTML += DescSortedArrow;
-                lastTh=th[i];
-                }
-            else th[i].innerHTML += NotSorted; // column not sorted
-
-            // click listener on col header
-            th[i].addEventListener('click', function (event) {
-                _this._sort(table, i, (dir = 1 - dir));
-
-                // add arrow to the sorted column
-                event.target.innerHTML = event.target.getAttribute('data-caption')
-                event.target.innerHTML += dir==1 ? DescSortedArrow : AscSortedArrow;
+        var HtmlTableSort = {
+            init: function( id,              // id of the table to make sortable
+                            NotSorted,       // text to display when not sorted
+                            AscSortedArrow,  // text to display when sorted asc
+                            DescSortedArrow  // text to display when sorted desc
+                            )
+                {
                 
-                // remove arrow from the other columns
-                if(lastTh && lastTh != event.target)
-                    {
-                    lastTh.innerHTML = lastTh.getAttribute('data-caption');
-                    lastTh.innerHTML += NotSorted; // column not sorted
-                    }
-                lastTh=event.target;
-            });
-        }(i));
-    },
-    _sort: function (table, col, reverse)
-        {   
-        var tb = table.tBodies[0], // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
-            tr = Array.prototype.slice.call(tb.rows, 0), // put rows into array
-            i;
+                var _this  = this; // store context of this in the object
+                var table  = document.getElementById(id);
+                var th     = table.tHead, i;
+                var lastTh =false;
+                
+                if (NotSorted === undefined)        NotSorted = '&nbsp;&middot';
+                if (AscSortedArrow === undefined)   AscSortedArrow = '&nbsp;&uarr;';
+                if (DescSortedArrow === undefined)  DescSortedArrow = '&nbsp;&darr;';
 
-        reverse = -((+reverse) || -1);
-
-        // sort rows
-        tr = tr.sort(function (a, b) {
-            // use attribute `data_sort_value` of exists for sorting (dates, numbers, ...)
-            va = a.cells[col].getAttribute('data-sort-value')==null ? a.cells[col].textContent.trim() : a.cells[col].getAttribute('data-sort-value');
-            vb = b.cells[col].getAttribute('data-sort-value')==null ? b.cells[col].textContent.trim() : b.cells[col].getAttribute('data-sort-value');
-
-            return reverse * ( va.localeCompare(vb,undefined, {numeric: true}) );
-            });
-
-        // append rows in new order
-        for(i = 0; i < tr.length; ++i)
-            tb.appendChild(tr[i]);
-    }
-};        
+                th && (th = th.rows[0]) && (th = th.cells);
+        
+                if(th)  i = th.length;
+                else    return; // no <thead then do nothing        
+        
+                // loop on <th>
+                while (--i >= 0) (function (i) {
+                    var dir = 1;
+        
+        
+                    // set & save column titles
+                    th[i].setAttribute('data-caption',th[i].innerHTML);
+                    var sort=th[i].getAttribute('data-sort');
+        
+                    // set cursor style
+                    if(sort!='0') th[i].style.cursor = 'pointer';
+        
+                    if(sort=='asc' || sort=='desc') // is it the default sorted column
+                        {                
+                        if(sort=='asc')
+                            {
+                            dir = 1 - dir;
+                            th[i].innerHTML += AscSortedArrow;
+                            }
+                        else th[i].innerHTML += DescSortedArrow;
+                        lastTh=th[i];
+                        }
+                    else if(sort!='0') th[i].innerHTML += NotSorted; // column not sorted
+        
+                    // click listener on col header
+                    if(sort!='0')
+                        th[i].addEventListener('click', function (event) {
+                            _this._sort(table, i, (dir = 1 - dir));
+        
+                            // add arrow to the sorted column
+                            event.target.innerHTML = event.target.getAttribute('data-caption')
+                            event.target.innerHTML += dir==1 ? DescSortedArrow : AscSortedArrow;
+                            
+                            // remove arrow from the other columns
+                            if(lastTh && lastTh != event.target)
+                                {
+                                lastTh.innerHTML = lastTh.getAttribute('data-caption');
+                                lastTh.innerHTML += NotSorted; // column not sorted
+                                }
+                            lastTh=event.target;
+                        });
+                }(i));
+            },
+            _sort: function (table, col, reverse)
+                {   
+                var tb = table.tBodies[0], // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
+                    tr = Array.prototype.slice.call(tb.rows, 0), // put rows into array
+                    i;
+        
+                reverse = -((+reverse) || -1);
+        
+                // sort rows
+                tr = tr.sort(function (a, b) {
+                    // use attribute `data_sort_value` of exists for sorting (dates, numbers, ...)
+                    va = a.cells[col].getAttribute('data-sort-value')==null ? a.cells[col].textContent.trim() : a.cells[col].getAttribute('data-sort-value');
+                    vb = b.cells[col].getAttribute('data-sort-value')==null ? b.cells[col].textContent.trim() : b.cells[col].getAttribute('data-sort-value');
+        
+                    return reverse * ( va.localeCompare(vb,undefined, {numeric: true}) );
+                    });
+        
+                // append rows in new order
+                for(i = 0; i < tr.length; ++i)
+                    tb.appendChild(tr[i]);
+            }
+        };        
+        
